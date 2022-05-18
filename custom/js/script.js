8c2742f02f92ff216d498f3f9129d1f98932f7ad
ihmwhydn <ihm245@gmail.com>
2022-05-18 11:41:07 +0700

    updt

--- custom/js/script.js
+++ custom/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+  new WOW().init();
+
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 83){
+      $(".navbar").removeClass('navbar-transparant shadow-none').addClass('navbar-theme shadow-sm');
+    } else{
+      $(".navbar").removeClass('navbar-theme shadow-sm').addClass('navbar-transparant shadow-none');
+    }
+  })
+
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     speed: 1000,
